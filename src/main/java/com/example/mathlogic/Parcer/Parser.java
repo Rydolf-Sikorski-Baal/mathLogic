22a5d80cf98a3e67c55cfe39ea9648b292a3ac6a
baal <rydolf_sikorski@mail.ru>
2022-12-24 19:38:21 +0300

    feat(parser): added finit states switch (unfinished)

--- src/main/java/com/example/mathlogic/Parcer/Parser.java
+++ src/main/java/com/example/mathlogic/Parcer/Parser.java
@@ -23,7 +23,7 @@ public class Parser {
     }
 
     private enum finiteStates {
-        START,
+        ANY_TYPE,
         SPACE,
         VARIABLE,
         OPERATOR,
@@ -44,20 +44,88 @@ public class Parser {
         return new ExpressionTree(root, new VariablesList());
     }
 
-    private class ExpressionSequence{}
-    private ExpressionSequence getExpressionSequenceFromString(String expression){
+    private class ExpressionSequence{
+        public void addElement(){}
+    }
+    private boolean isEmpty(char ch){return (ch == ' ') || (ch == '\n');}
+    private boolean isCorrectVariableSymbol(char ch){return ch == 'a';}
+    private ExpressionSequence getExpressionSequenceFromString(char[] expression){
         ExpressionSequence expressionSequence = new ExpressionSequence();
+
+        int ind = 0;
+
+        finiteStates state = finiteStates.START;
+        while(state != finiteStates.FINISH)
+            switch(state){
+                case ANY_TYPE:
+                    /* необходимо определить тип первого символа (конец строки -> finish) */
+                    switch(expression[ind]){
+                        case '(':
+                            state = finiteStates.OPENING_BRACES;
+                            break;
+                        case ')':
+                            state = finiteStates.CLOSING_BRACES;
+                            break;
+                        case '-':
+                        case '|':
+                        case '&':
+                        case '!':
+                            state = finiteStates.OPERATOR;
+                            break;
+                    }
+                    state = finiteStates.FINISH;
+                    break;
+                case SPACE:
+                    while (isEmpty(expression[ind])) ind++;
+
+                    state = finiteStates.ANY_TYPE;
+
+                    break;
+                case VARIABLE:
+                    StringBuilder name = new StringBuilder();
+                    while (isCorrectVariableSymbol(expression[ind])){
+                        name.append(expression[ind]);
+                        ind++;
+                    }
+
+                    /* добавить переменную в множество переменных */
+
+                    state = finiteStates.ANY_TYPE;
+                    break;
+                case OPERATOR:
+                    if (expression[ind] == '!'){/* добавить элемент в выражение */ ind++;   break;}
+                    if (expression[ind] == '|'){/* добавить элемент в выражение */ ind++;   break;}
+                    if (expression[ind] == '&'){/* добавить элемент в выражение */ ind++;   break;}
+                    if (expression[ind] == '-'){/* добавить элемент в выражение */ ind +=2; break;}
+
+                    state = finiteStates.ANY_TYPE;
+                    break;
+                case OPENING_BRACES:
+                    /* просто занести в выражение и сменить тип */
+
+                    state = finiteStates.ANY_TYPE;
+                    break;
+                case CLOSING_BRACES:
+                    /* просто занести в выражение и сменить тип */
+
+                    state = finiteStates.ANY_TYPE;
+                    break;
+            }
 
         return expressionSequence;
     }
 
     private ExpressionTreeNode getTreeRootFromSequence(){
-        String nodeType = "";/* SparseTable  */.getMaxFromSequence(int left, int right);
+        String nodeType = "";/* SparseTable  */.getMaxIndexFromSequence(int left, int right);
 
         ExpressionTreeNode node = null;
         switch(nodeType){
+            /* различные варианты узлов */
             case("logicInversion") -> {node = new UnaryOperationNode(new LogicInversion()); break;}
             default -> {node = null;}
         }
+
+        /* вызов этой функции рекурсивно если полученная вершина соответствует операции
+        (один или два вызова в зависимости от типа операции) */
     }
 }
